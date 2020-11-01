7cd037e37def577b9215da8446a387ba2e41a608
martin <martin@voyager.intra.zimmermann.family>
2020-11-01 17:02:17 +0100

    More initial configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -135,7 +135,7 @@
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+#define MACHINE_UUID "380c2f91-b685-4908-bcc1-b647d924498a"
 
 // @section extruder
 
